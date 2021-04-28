7901532a6d6a7b39c7fb7ae5853717df39b38909
TheGrandestNothing <zhilyaev.slava@mail.ru>
2021-04-28 15:04:36 +0500

    bootstrap changed

--- WebApp/ClientApp/src/components/CartDesctiption.js
+++ WebApp/ClientApp/src/components/CartDesctiption.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { LoginMenu } from './api-authorization/LoginMenu';
 import "./CartDescription.css";
 import { ApplicationPaths } from './api-authorization/ApiAuthorizationConstants';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap'
+import cart from '../Data/cart.svg'
 
 export class CartDescription extends Component {
 
@@ -17,15 +19,17 @@ export class CartDescription extends Component {
     render() {
         return (
             <div className="info">
-                <p>{this.state.CartName}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="black" class="bi bi-cart" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                </svg>
-                <p>Общая средняя стоимость:</p>
-                <p>Максимальное кол-во товаров в одном магазине:</p>
-                <button type="button" class="btn btn-primary">Сохранить</button>
-                <button type="button" class="btn btn-primary">Расчитать</button>
-                <button type="button" class="btn btn-primary">Поделиться</button>
+                <Card>
+                    <CardTitle tag="h5">{this.state.CartName}</CardTitle>
+                    <CardImg top width="100%" src={cart} alt="Card image cap" />
+                    <CardBody>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Общая средняя стоимость </CardSubtitle>
+                        <CardSubtitle tag="h6" className="mb-2 text-muted">Максимальное кол-во товаров в одном магаине</CardSubtitle>
+                        <Button type="button" class="btn btn-primary">Сохранить</Button>
+                        <Button type="button" class="btn btn-primary">Расчитать</Button>
+                        <Button type="button" class="btn btn-primary">Поделиться</Button>
+                    </CardBody>
+                </Card>
             </div>
         );
     }
--- WebApp/ClientApp/src/components/Home.js
+++ WebApp/ClientApp/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import "./Home.css";
 import { ApplicationPaths } from './api-authorization/ApiAuthorizationConstants';
+import { Card, CardImg, CardBody, CardTitle } from 'reactstrap'
+import user from '../Data/user.svg'
+import cart from '../Data/cart.svg'
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -22,29 +25,31 @@ export class Home extends Component {
 
   authenticatedView() {
     return (
-      <a href={ApplicationPaths.Cart}>
-        <div className="mainDiv">
-          <svg className="userPic" xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="black" class="bi bi-cart" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-          </svg>
-          <p>Создать корзину</p>
-        </div>
-      </a>
+      <div className="main">
+        <a href={ApplicationPaths.Cart}>
+          <Card>
+            <CardImg top width="100%" src={cart} alt="Card image cap" />
+            <CardBody>
+              <CardTitle tag="h5">Создать корзину</CardTitle>
+            </CardBody>
+          </Card>
+        </a>
+      </div>
     );
   }
 
   notAuthenticatedView() {
     return (
-      <a href={ApplicationPaths.Register}>
-        <div className="mainDiv">
-          <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="black" class="bi bi-person-fill" viewBox="0 0 16 16">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-          </svg>
-          <p>Зарегистрироваться или войти</p>
-        </div>
-      </a>
+      <div className="main">
+        <a href={ApplicationPaths.Register}>
+          <Card>
+            <CardImg top width="100%" src={user} alt="Card image cap" />
+            <CardBody>
+              <CardTitle tag="h5">Зарегистрироваться или войти</CardTitle>
+            </CardBody>
+          </Card>
+        </a>
+      </div>
     )
   }
-
-
 }
